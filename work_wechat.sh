50a9691565fa0c02e3872c557595b5f0a3d15fa1
songwudea <songwudea@gmail.com>
2023-10-30 16:27:36 +0800

    1
    1

--- work_wechat.sh
+++ work_wechat.sh
@@ -4,12 +4,10 @@ curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=693axxx6-7aoc-4bc4-97
    -H 'Content-Type: application/json' \
    -d '
    {
-    "msgtype": "markdown",
-    "markdown": {
-        "content": "实时新增用户反馈<font color=\"warning\">132例</font>，请相关同事注意。\n
-         >类型:<font color=\"comment\">用户反馈</font>
-         >普通用户反馈:<font color=\"comment\">117例</font>
-         >VIP用户反馈:<font color=\"comment\">15例</font>"
-    }
-}
+        "msgtype": "text",
+        "text": {
+            "content": "hello world"
+        }
+   }'
+
 
