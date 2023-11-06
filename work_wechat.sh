fad3b91a595815c6c01a8605aea912d15399661c
songwudea <songwudea@gmail.com>
2023-11-06 13:47:25 +0800

    Update work_wechat.sh

--- work_wechat.sh
+++ work_wechat.sh
@@ -6,10 +6,7 @@ curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=349d6b14-af2c-4c1e-89
    {
     "msgtype": "text",
     "text": {
-        "content": "深圳今日天气：29度，大部分多云，降雨概率：60%",
-        "mentioned_list":["wangqing","@all"],
-        "mentioned_mobile_list":["13800001111","@all"]
+        "content": "请大家记得写周报哦",
+        "mentioned_list":[@all"]
     }
 }'
-
-
